8703fc6557221fb983c004e6ddb34fda1afc41d7
SIMULATAN <SIMULATAN>
2020-06-05 21:48:12 +0200

    Imgurmeme hinzugefügt

--- DiscordBot.Modules/PingModule.cs
+++ DiscordBot.Modules/PingModule.cs
@@ -4,22 +4,12 @@ using System.Threading.Tasks;
 
 namespace DiscordBot.Modules
 {
-    //https://api.thecatapi.com/v1/images/search?breed_ids=abys
     public class PingModule : ModuleBase
     {
         [Command("ping")]
         public async Task TestCommandAsync()
         {
             await ReplyAsync("Pong!");
-        }
-
-        [Command("ping2")]
-        public async Task Ping2() => await TestCommandAsync();
-
-        [Command("hello")]
-        public async Task HelloWorld()
-        {
-            await ReplyAsync("World!");
         }
     }
 }
