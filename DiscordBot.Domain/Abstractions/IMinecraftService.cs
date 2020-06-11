958d1437feb35a90a5aee47fe4e1a0c2b2872eb2
David Nemecek <40146720160278@litec.ac.at>
2020-06-12 00:00:12 +0200

    a

---
+++ DiscordBot.Domain/Abstractions/IMinecraftService.cs
@@ -0,0 +1,10 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot.Modules.Services
+{
+    public interface IMinecraftService
+    {
+        Task<string> ExecuteCommandAsync(string command);
+    }
+}
---
+++ DiscordBot.Modules/Services/MinecraftService.cs
@@ -0,0 +1,60 @@
+﻿using LibMCRcon.RCon;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using DiscordBot.Domain.Configuration;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+
+namespace DiscordBot.Modules.Services
+{
+    public class MinecraftService : IMinecraftService
+    {
+        private TCPRcon rcon;
+        private MinecraftSettings settings;
+
+        public MinecraftService(IOptions<MinecraftSettings> s)
+        {
+            settings = s.Value;
+            EnsureConnectedAsync();
+        }
+
+        public void Dispose()
+        {
+            rcon.StopComms();
+        }
+
+        public async Task<string> ExecuteCommandAsync(string command)
+        {
+            await EnsureConnectedAsync();
+            if (!rcon.IsConnected)
+            {
+                return "No Connection to the given Server available.";
+            }
+
+            return await rcon.ExecuteCmdAsync(command);
+        }
+
+        private async ValueTask EnsureConnectedAsync()
+        {
+            if (rcon != null && rcon.IsConnected)
+            {
+                return;
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (rcon != null && rcon.IsConnected)
+                {
+                    return;
+                }
+
+                rcon = new TCPRcon(settings.IP, settings.Port, settings.Password);
+                rcon.StartComms();
+                await Task.Delay(5000);
+            }
+
+            return;
+        }
+    }
+}
