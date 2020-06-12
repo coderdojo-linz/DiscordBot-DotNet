fc2d301c4a66b14d016c70763cd04cad77123333
DavidNemecek <38526555+DavidNemecek@users.noreply.github.com>
2020-06-12 17:27:13 +0200

    fet-WeatherReaction (#26)
    * a
    
    * Renamed IImageService to IWeatherService
    Added GetTemperature to IWeatherService and WeatherService
    WeatherService now returnes Picture and Temerature
    
    * Changed lowecase functionname
    checked if weatherIcon is null
    
    * tidied up GetWeatherStream
    
    Co-authored-by: David Nemecek <40146720160278@litec.ac.at>

--- DiscordBot.Modules/Services/WeatherService.cs
+++ DiscordBot.Modules/Services/WeatherService.cs
@@ -3,13 +3,18 @@ using DiscordBot.Domain.WeatherModel;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
+using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DiscordBot.Modules.Services
 {
+    /// <summary>
+    /// Returns Weather Information from the privided Location
+    /// </summary>
     public class WeatherService : IWeatherService
     {
         private readonly HttpClient _httpClient;
@@ -19,21 +24,47 @@ namespace DiscordBot.Modules.Services
             _httpClient = httpClient.CreateClient();
         }
 
-
-
+        /// <summary>
+        /// Get Picture of current weather
+        /// </summary>
+        /// <param name="location">Location of the picture</param>
+        /// <returns>Stream of the Picture</returns>
         public async Task<Stream> GetWeatherStream(string location)
         {
-            //Get Weather from the API
+            try
+            {
+                //Get Weather from the API
+                var weather = await GetOpenWeatherMapModuleByLocation(location);
+
+                //Get Picture
+                if ((weather?.Weather?.Length ?? 0) == 0 || weather.Weather[0].Icon == null)
+                {
+                    return null;
+                }
+                string weatherIcon = weather.Weather[0].Icon;
+
+                var pictureResponse = await _httpClient.GetAsync($"http://openweathermap.org/img/wn/{weatherIcon}@2x.png");
+                //Send back to Discord Server
+                return await pictureResponse.Content.ReadAsStreamAsync();
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+
+        }
+
+        /// <summary>
+        /// Get Temperature at the given Location
+        /// </summary>
+        /// <param name="location">Location of the Temperature</param>
+        /// <returns>Temperature</returns>
+        public async Task<double> GetTemperature(string location) => (await GetOpenWeatherMapModuleByLocation(location)).Main.Temp;
+
+        private async Task<OpenWeatherMapModel> GetOpenWeatherMapModuleByLocation(string location)
+        {
             var result = await _httpClient.GetAsync($"http://api.openweathermap.org/data/2.5/weather?q={location}&appid=ab2446e861742c6758a49c789d0f4e6a");
-            var weather = JsonConvert.DeserializeObject<OpenWeatherMapModel>(await result.Content.ReadAsStringAsync());
-
-            //Get Picture
-            var pictureResponse = await _httpClient.GetAsync($"http://openweathermap.org/img/wn/{weather.Weather[0].Icon}@2x.png");
-
-            //Send back to Discord Server
-            return await pictureResponse.Content.ReadAsStreamAsync();
-
-
+            return JsonConvert.DeserializeObject<OpenWeatherMapModel>(await result.Content.ReadAsStringAsync());
         }
     }
 }
