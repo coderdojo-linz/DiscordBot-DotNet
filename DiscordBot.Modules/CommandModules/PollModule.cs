da6723a71b8a89494c95d605b38205cf48851be7
Jonas Kamsker <11245306+JKamsker@users.noreply.github.com>
2020-06-13 14:40:52 +0200

    .

---
+++ DiscordBot.Modules/CommandModules/PollModule.cs
@@ -0,0 +1,61 @@
+﻿using Discord;
+using Discord.Commands;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot.Modules.CommandModules
+{
+    //-poll "Eins" "Zweites Zwei" "Drittens"
+    public class PollModule : ModuleBase<SocketCommandContext>
+    {
+        public PollModule()
+        {
+        }
+
+        [Command("poll")]
+        public async Task CreatePollAsync(string title, params string[] options)
+        {
+            var emojis = new[]
+            {
+                "zero",
+                "one",
+                "two",
+                "three",
+                "four",
+                "five",
+                "six",
+                "seven",
+                "eight",
+                "nine",
+                "keycap_ten",
+            }
+                .Select(x => GEmojiSharp.Emoji.Get(x))
+                .ToList();
+
+            var builder = new EmbedBuilder()
+                .WithColor(Color.DarkRed)
+                .WithTitle(title);
+
+            var sb = new StringBuilder().AppendLine(" ");
+            foreach (var (option, emoji) in options.Zip(emojis, (x, y) => (x, y)))
+            {
+                sb.AppendLine($"{emoji.Raw}: {option}").AppendLine();
+            }
+
+            builder.AddField("Add a reaction to perform your vote", sb);
+
+            var res = builder.Build();
+
+            var msg = await ReplyAsync(embed: res);
+
+            foreach (var reaction in emojis.Take(options.Length))
+            {
+                await msg.AddReactionAsync(new Emoji(reaction.Raw));
+            }
+
+            await base.Context.Message.DeleteAsync();
+        }
+    }
+}
