a0a4ce319e0e99bfa24f6efcc28087d3dc0a52b4
Jonas Kamsker <jonas.kamsker@lints.at>
2020-06-15 11:48:59 +0200

    Improved dice command

--- DiscordBot.Modules/CommandModules/DiceModule.cs
+++ DiscordBot.Modules/CommandModules/DiceModule.cs
@@ -1,8 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using Discord.Commands;
+﻿using Discord.Commands;
+
 using Microsoft.Extensions.Logging;
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace DiscordBot.Modules.CommandModules
 {
@@ -32,15 +35,12 @@ namespace DiscordBot.Modules.CommandModules
             for (int i = 1; i < 6; i++)
             {
                 unicodeEmojis.Add(GEmojiSharp.Emoji.Get(emojis[i]).Raw);
-            }                
-            int random = randomGen.Next(1, 6);
-            Discord.IUserMessage msg = await Context.Channel.SendMessageAsync($":game_die: Du hast die Zahl {unicodeEmojis[random-1]} gewürfelt!");
-            for (int i = 1; i < 5; i++)
-            {
-                random = randomGen.Next(1, 6);
-                await msg.ModifyAsync(msg => msg.Content = $":game_die: Du hast die Zahl {unicodeEmojis[random-1]} gewürfelt!");
-                await Task.Delay(500);
             }
+            int random = randomGen.Next(1, 6);
+            var msg = await Context.Channel.SendMessageAsync($"Ein zufälliger Algorithmus würfelt.... :game_die: <a:rotating:722023788736151623>");
+
+            await Task.Delay(TimeSpan.FromSeconds(5));
+            await msg.ModifyAsync(msg => msg.Content = $":game_die: Du hast die Zahl {unicodeEmojis[random - 1]} gewürfelt!");
             logger.LogInformation($"{Context.User} hat die Zahl {random + 1} gewürfelt!");
         }
     }
