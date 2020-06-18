3f19fab90c7b922596591faece1ca2817ed9bfe7
SIMULATAN <SIMULATAN>
2020-06-18 14:22:44 +0200

    Merged Jakob's CatModule into the main CatModule

--- DiscordBot.Modules/CommandModules/CatModule.cs
+++ DiscordBot.Modules/CommandModules/CatModule.cs
@@ -1,21 +1,100 @@
-﻿using Discord.Commands;
+﻿using Discord;
+using Discord.Commands;
 
-using DiscordBot.Modules.Services;
-
+using Newtonsoft.Json;
+using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using DiscordBot.Domain.Configuration;
 
-namespace DiscordBot.Modules.CommandModules
+namespace DiscordBot.Modules
 {
     public class CatModule : ModuleBase
     {
-        private readonly ICatService _catService;
+        public CatModule(IOptions<DiscordSettings> options)
+        {
+            string prefix = options.Value.CommandPrefix;
+            _prefix = prefix;
+        }
 
-        public CatModule(ICatService catService) => _catService = catService;
+        string _prefix;
+        [Command("catexamples")]
+        [Summary("Gibt dir Beispiele für Katzenarten aus")]
+        public async Task Catexamples()
+        {
+            var embed = new EmbedBuilder();
+            embed.WithFooter(footer => footer.Text = $"{_prefix}catexamples")
+                .WithColor(Color.DarkRed)
+                .AddField("Beispiele für Katzen", "Bengal - Beng \n Abyssian - Abys \n Britischer Kurzhaar - bsho \n Toyger - toyg \n sava - Savannah \n Europäischer Burmese - ebur \n Ocikatze - ocic \n Maine Coon - mcoo")
+                .WithCurrentTimestamp();
+            await Context.Channel.SendMessageAsync("", false, embed.Build());
+        }
 
-        [Command("cat"), Summary("Shows us cats")]
-        public async Task Cat() => await base.Context.Channel.SendFileAsync(await _catService.GetCatAsync(), "cat.jpg");
+        [Command("cat", RunMode = RunMode.Async)]
+        [Summary("Zeigt dir eine süße Katze ^-^")]
+        public async Task Cat([Remainder] string input = "nothing")
+        {
+            int time = 0;
+            HttpClient httpClient = new HttpClient();
+            string query = "https://api.thecatapi.com/v1/images/search?breed_ids=" + input;
+            HttpResponseMessage message = await httpClient.GetAsync(query);
+            string response = await message.Content.ReadAsStringAsync();
 
-        [Command("more-cat"), Summary("Shows us more cats")]
-        public async Task MoreCat() => await base.Context.Channel.SendFileAsync(await _catService.GetCatAsync(), "cat.jpg");
+            if (response == "[]" || input == "nothing")
+            {
+                if (input != "nothing")
+                {
+                    await ReplyAsync($"Es konnte keine Katze der Art ``{input}`` gefunden werden!");
+                    await Task.Delay(1500);
+                    await ReplyAsync("Hier bekommst du trotzdem eine zufällige Katze <:cutecat:720278063409004565>");
+                    time = 1000;
+                }
+
+                HttpResponseMessage message2 = await httpClient.GetAsync("https://api.thecatapi.com/v1/images/search");
+                string response2 = await message2.Content.ReadAsStringAsync();
+
+                TheCatApiResponse deserialized2 = JsonConvert.DeserializeObject<TheCatApiResponse[]>(response2)[0];
+
+                var url2 = deserialized2.Url;
+                HttpResponseMessage assyncurl2 = await httpClient.GetAsync(url2);
+
+                var streamResponse2 = await assyncurl2.Content.ReadAsStreamAsync();
+                await Task.Delay(time);
+                await base.Context.Channel.SendFileAsync(streamResponse2, "cat.jpg");
+            }
+            else
+            {
+                TheCatApiResponse deserialized = JsonConvert.DeserializeObject<TheCatApiResponse[]>(response)[0];
+
+                var url = deserialized.Url;
+                HttpResponseMessage asyncurl = await httpClient.GetAsync(url);
+
+                var streamResponse = await asyncurl.Content.ReadAsStreamAsync();
+                await base.Context.Channel.SendFileAsync(streamResponse, "cat.jpg");
+            }
+        }
+
+        [Command("catgif")]
+        [Alias("gifcat")]
+        [Summary("Schickt dir eine animierte Katze ^-^")]
+        public async Task Catgif()
+        {
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage message = await httpClient.GetAsync("https://api.thecatapi.com/v1/images/search?mime_types=gif");
+            string response = await message.Content.ReadAsStringAsync();
+
+            var deserialized = JsonConvert.DeserializeObject<TheCatApiResponse[]>(response).FirstOrDefault();
+            var url = deserialized.Url;
+
+            HttpResponseMessage message2 = await httpClient.GetAsync(url);
+            var streamResponse = await message2.Content.ReadAsStreamAsync();
+            await base.Context.Channel.SendFileAsync(streamResponse, "cat.gif");
+        }
+        public class TheCatApiResponse
+        {
+            [JsonProperty("url")]
+            public string Url { get; set; }
+        }
     }
 }
