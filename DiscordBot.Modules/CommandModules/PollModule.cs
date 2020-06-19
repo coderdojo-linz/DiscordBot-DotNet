313262e259baab1cd8c9845d533446abe05902cf
Jonas Kamsker <jonas.kamsker@lints.at>
2020-06-19 19:15:33 +0200

    added db to pollmodule

--- DiscordBot.Modules/CommandModules/PollModule.cs
+++ DiscordBot.Modules/CommandModules/PollModule.cs
@@ -1,20 +1,54 @@
-﻿using System;
+﻿using Discord;
+using Discord.Commands;
+
+using DiscordBot.Domain.Configuration;
+
+using Microsoft.Extensions.Options;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Text;
 using System.Text.RegularExpressions;
-using Discord;
-using Discord.Commands;
-using DiscordBot.Domain.Configuration;
-using Microsoft.Extensions.Options;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace DiscordBot.Modules.CommandModules
 {
+
+
+
+    public class PollInfo
+    {
+        public ulong MessageId { get; set; }
+        public ulong UserId { get; set; }
+        public List<OptionEmojiPair> Options { get; set; } = new List<OptionEmojiPair>();
+    }
+
+    public class OptionEmojiPair
+    {
+        public string Option { get; set; }
+        public string Emoji { get; set; }
+    }
+
     public class PollModule : ModuleBase
     {
-        string _prefix;
-        public PollModule(IOptions<DiscordSettings> options)
+        private readonly IConfiguration _configuration;
+        private readonly ILogger<FileExplorerModule> _logger;
+        private string _prefix;
+
+        public PollModule
+        (
+            IOptions<DiscordSettings> options, 
+            IConfiguration configuration,
+            ILogger<FileExplorerModule> logger 
+        )
         {
+            _configuration = configuration;
+            _logger = logger;
             string prefix = options.Value.CommandPrefix;
             _prefix = prefix;
         }
@@ -46,6 +80,20 @@ namespace DiscordBot.Modules.CommandModules
                 .Select(emoji => GEmojiSharp.Emoji.Get(emoji).Raw)
                 .ToList();
 
+            var pollInfo = new PollInfo()
+            {
+                UserId = base.Context.User.Id
+            };
+
+            for (int i = 0; i < options.Length; i++)
+            {
+                pollInfo.Options.Add(new OptionEmojiPair
+                {
+                    Emoji = emojis[i],
+                    Option = options[i]
+                });
+            }
+
             var builder = new EmbedBuilder()
                 .WithColor(Color.DarkRed)
                 .WithFooter(footer => footer.Text = $"{_prefix}poll")
@@ -53,19 +101,77 @@ namespace DiscordBot.Modules.CommandModules
                 .WithTitle(title);
 
             StringBuilder desc = new StringBuilder();
-            for (int i = 0; i < options.Length; i++)
+            foreach (var emojiPair in pollInfo.Options)
             {
-                desc.AppendLine($"{emojis[i]} - {options[i]}\n");
+                desc.AppendLine($"{emojiPair.Emoji} - {emojiPair.Option}\n");
             }
+
             builder.AddField("Reagiere mit einem Emoji um zu voten:", desc.ToString());
 
             //await base.Context.Message.DeleteAsync();
             IUserMessage message = await base.Context.Channel.SendMessageAsync(embed: builder.Build());
+            pollInfo.MessageId = message.Id;
+
             for (int i = 0; i < options.Length; i++)
             {
                 await message.AddReactionAsync(new Emoji(emojis[i]));
             }
+
+            //Save database
+            await AddEntryToDatabase(pollInfo);
         }
+
+        /// <summary>
+        /// 1. LoadDatabase
+        /// 2. Append Data
+        /// 3. Save Database
+        /// </summary>
+        /// <returns></returns>
+        public async Task AddEntryToDatabase(PollInfo pollInfo)
+        {
+            var db = await ReadDatabase();
+            db.Add(pollInfo);
+
+            var filesFolder = _configuration["FileStore:DataFolder"];
+            if (!Directory.Exists(filesFolder))
+            {
+                _logger.LogError("FileStore:DataFolder does not refer to an existing directory. Did you configure it in appsettings.json?");
+                await ReplyAsync("Bot not properly configured. Check logs for details.");
+                return;
+            }
+
+            var pollModuleDirectory = Directory.CreateDirectory(Path.Combine(filesFolder, "PollModule"));
+            var serialized = JsonConvert.SerializeObject(db);
+            await File.WriteAllTextAsync(Path.Combine(pollModuleDirectory.FullName, "PollDb.json"), serialized);
+        }
+
+        public async Task<List<PollInfo>> ReadDatabase()
+        {
+            var filesFolder = _configuration["FileStore:DataFolder"];
+            if (!Directory.Exists(filesFolder))
+            {
+                _logger.LogError("FileStore:DataFolder does not refer to an existing directory. Did you configure it in appsettings.json?");
+                await ReplyAsync("Bot not properly configured. Check logs for details.");
+                return null;
+            }
+
+            var pollModuleDirectory = Directory.CreateDirectory(Path.Combine(filesFolder, "PollModule"));
+            if (!File.Exists(Path.Combine(pollModuleDirectory.FullName, "PollDb.json")))
+            {
+                return new List<PollInfo>();
+            }
+
+            var content = await File.ReadAllTextAsync(Path.Combine(pollModuleDirectory.FullName, "PollDb.json"));
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<List<PollInfo>>(content);
+
+        }
+
+        public async Task<PollInfo> GetPollInfoForMessage(ulong messageId)
+        {
+            var db = await ReadDatabase();
+            return db.Where(x => x.MessageId == messageId).FirstOrDefault();
+        } 
+
 
         [Command("endpoll")]
         public async Task EndPollAsync(string idOrUrl, [Remainder] string message = "")
