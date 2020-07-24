6da7cf0f40dc51758c9b568a2d881c813e87caf7
blaumeise20 <62756994+blaumeise20@users.noreply.github.com>
2020-07-24 14:59:44 +0200

    Colorize weather embed
    Calcs the color using the temperature

--- DiscordBot.Modules/CommandModules/WeatherModule.cs
+++ DiscordBot.Modules/CommandModules/WeatherModule.cs
@@ -3,6 +3,7 @@ using Discord.Commands;
 
 using DiscordBot.Domain.Abstractions;
 
+using System;
 using System.IO;
 using System.Threading.Tasks;
 
@@ -54,6 +55,7 @@ namespace DiscordBot.Modules.CommandModules
                 .WithThumbnailUrl(weather.ThumbnailUrl)
                 .WithTitle(weather.Main)
                 .WithDescription(weather.Description)
+                .WithColor(GetEmbedColor((float)weather.TempAsDouble))
                 .AddField("Temparatur", weather.Temparature, true)
                 .AddField("RealFeel", weather.RealFeelTemp, true)
                 .AddField("\u200b", "\u200b", false)
@@ -62,6 +64,43 @@ namespace DiscordBot.Modules.CommandModules
                 ;
 
             await ReplyAsync(embed: eb.Build());
+        }
+
+        private Color GetEmbedColor(float temp)
+        {
+            float minTemp = -5;
+            float maxTemp = 40;
+            float span = maxTemp - minTemp;
+
+            temp = (float)Limit(minTemp, temp, maxTemp);
+            float partOfColor = (float)Math.Round((temp - minTemp) / span, digits: 3);
+            var startColor = (r: 52, g: 152, b: 219);
+            var endColor = (r: 231, g: 76, b: 60);
+
+            float red = CalcColorPart(startColor.r, endColor.r, partOfColor);
+            float green = CalcColorPart(startColor.g, endColor.g, partOfColor);
+            float blue = CalcColorPart(startColor.b, endColor.b, partOfColor);
+            
+            return new Color((int)Limit(0, Math.Round(red), 255), (int)Limit(0, Math.Round(green), 255), (int)Limit(0, Math.Round(blue), 255));
+        }
+
+        private double Limit(double min, double given, double max)
+        {
+            return Math.Max(min, Math.Min(given, max));
+        }
+
+        private float CalcColorPart(float value1, float value2, float partOfColor)
+        {
+            float span = Math.Abs(value2 - value1);
+            float partOfSpan = partOfColor * span;
+            if (value2 > value1)
+            {
+                return value1 + partOfSpan;
+            }
+            else
+            {
+                return value1 - partOfSpan;
+            }
         }
     }
 }
