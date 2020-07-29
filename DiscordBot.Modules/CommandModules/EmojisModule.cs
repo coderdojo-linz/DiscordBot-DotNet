6477bb4c8588a2c37cd5ec0dfa6b83dfaf8d9301
blaumeise20 <62756994+blaumeise20@users.noreply.github.com>
2020-07-29 12:15:52 +0200

    Simplify EmojisModule

--- DiscordBot.Modules/CommandModules/EmojisModule.cs
+++ DiscordBot.Modules/CommandModules/EmojisModule.cs
@@ -1,84 +1,51 @@
 ﻿using Discord;
-using Discord.Commands;
 using Discord.Webhook;
-
-
-using System.Text;
+using Discord.Commands;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DiscordBot.Modules
 {
-    [Group("emoji")]
-    [Alias("em")]
     public class EmojisModule : ModuleBase
     {
-        [Command("sans")]
-        public async Task Sans()
+        internal static Dictionary<string, string> Emojis { get; set; } = new Dictionary<string, string>()
         {
-            await SendEmojiWithName("sans");
-        }
-        [Command("thonk")]
-        public async Task Thonk()
-        {
-            await SendEmojiWithName("thonk");
-        }
-        [Command("mindblowing")]
-        public async Task Mindblowing()
-        {
-            await SendEmojiWithName("mindblowing");
-        }
-        [Command("rotatethink")]
-        public async Task Rotatethink()
-        {
-            await SendEmojiWithName("rotatethink");
-        }
-        [Command("hyper")]
-        public async Task Hyper()
-        {
-            await SendEmojiWithName("hyper");
-        }
-        [Command("xd")]
-        public async Task Xd()
-        {
-            await SendEmojiWithName("xd");
-        }
-        [Command("rainbowcat")]
-        public async Task Rainbowcat()
-        {
-            await SendEmojiWithName("rainbowcat");
-        }
-        [Command("yes")]
-        public async Task Yes()
-        {
-            await SendEmojiWithName("yes");
-        }
-        [Command("no")]
-        public async Task No()
-        {
-            await SendEmojiWithName("no");
-        }
-        [Command("calculated")]
-        public async Task Calculated()
-        {
-            await SendEmojiWithName("calculated");
-        }
+            ["sans"] = "<a:sans:719488632104288286>",
+            ["thonk"] = "<:thonk:718853430969368637>",
+            ["mindblowing"] = "<:mindblowing:719492411369324584>",
+            ["rotatethink"] = "<a:rotatethonk:719491269885427752>",
+            ["hyper"] = "<a:hyper:719494595313926244>",
+            ["xd"] = "<:xd:719510255402352690>",
+            ["rainbowcat"] = "<a:catrainbow1:719513824511656037><a:catrainbow2:719513824553861121>\n<a:catrainbow3:719513824905920603><a:catrainbow4:719513825040400415>",
+            ["yes"] = "<a:yes:719496630902063149>",
+            ["no"] = "<a:no:719496639471157288>",
+            ["calculated"] = "<:calculated:719518217730654218>"
+        };
 
-        private async Task SendEmojiWithName(string name)
+        // If you change the emojis, don't forget to also change the description!!
+        [Description(@"
+            Verfügbare Emojis:
+            - sans (<a:sans:719488632104288286>)
+            - thonk (<:thonk:718853430969368637>)
+            - mindblowing (<:mindblowing:719492411369324584>)
+            - rotatethink (<a:rotatethonk:719491269885427752>)
+            - hyper (<a:hyper:719494595313926244>)
+            - xd (<:xd:719510255402352690>)
+            - rainbowcat (keine Vorschau)
+            - yes (<a:yes:719496630902063149>)
+            - no (<a:no:719496639471157288>)
+            - calculated (<:calculated:719518217730654218>)
+        ")]
+        [Command("emoji")]
+        [Alias("em")]
+        public async Task SendEmojiWithName(string name)
         {
-            string emoji = name switch
+            if (!Emojis.TryGetValue(name, out string emoji))
             {
-                "sans" => "<a:sans:719488632104288286>",
-                "thonk" => "<:thonk:718853430969368637>",
-                "mindblowing" => "<:mindblowing:719492411369324584>",
-                "rotatethink" => "<a:rotatethonk:719491269885427752>",
-                "hyper" => "<a:hyper:719494595313926244>",
-                "xd" => "<:xd:719510255402352690>",
-                "rainbowcat" => "<a:catrainbow1:719513824511656037><a:catrainbow2:719513824553861121>\n<a:catrainbow3:719513824905920603><a:catrainbow4:719513825040400415>",
-                "yes" => "<a:yes:719496630902063149>",
-                "no" => "<a:no:719496639471157288>",
-                "calculated" => "<:calculated:719518217730654218>",
-                _ => "unknown"
-            };
+                await ReplyAsync($"Das Emoji {name} wurde nicht gefunden!");
+                return;
+            }
 
             var cxt = base.Context;
             var webhook = await ((ITextChannel)cxt.Channel).CreateWebhookAsync(((IGuildUser)cxt.User).Nickname ?? cxt.User.Username);
