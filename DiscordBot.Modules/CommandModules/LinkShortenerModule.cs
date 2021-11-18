8212c01e602cd51b449219582b38d2f6c5317c27
Jonas Kamsker <git.jonas@kamsker.at>
2021-11-18 23:52:58 +0100

    Added "Create"

--- DiscordBot.Modules/CommandModules/LinkShortenerModule.cs
+++ DiscordBot.Modules/CommandModules/LinkShortenerModule.cs
@@ -11,6 +11,8 @@ using DiscordBot.Modules.Services;
 
 namespace DiscordBot.Modules.CommandModules
 {
+    [Group("linkShortener")]
+    [Summary("Kürzt einen link auf ``meet.coderdojo.net``")]
     public class LinkShortenerModule : ModuleBase
     {
         private readonly LinkShortenerService _linkShortenerService;
@@ -23,7 +25,7 @@ namespace DiscordBot.Modules.CommandModules
         }
 
         [Command("update")]
-        public async Task UpdateLinkAsync(string id, [Remainder]string link)
+        public async Task UpdateLinkAsync(string id, [Remainder] string link)
         {
             var user = base.Context.User;
             if (user is not IGuildUser gu)
@@ -39,10 +41,33 @@ namespace DiscordBot.Modules.CommandModules
             }
 
             //base.Context.Guild.user
-           
+
             await Context.Channel.SendMessageAsync($"Updating {id} to {link}!");
             await _linkShortenerService.UpdateUrlAsync(id, _settings.AccessKey, link);
             await Context.Channel.SendMessageAsync($"Update complete!");
+        }
+
+        [Command("create")]
+        public async Task CreateLinkAsync(string id, [Remainder] string link)
+        {
+            var user = base.Context.User;
+            if (user is not IGuildUser gu)
+            {
+                await ReplyAsync("https://flukyfeed.com/wp-content/uploads/2020/07/wait-a-minute.jpg");
+                return;
+            }
+
+            if (!gu.GuildPermissions.Administrator)
+            {
+                await ReplyAsync("https://img-9gag-fun.9cache.com/photo/a1rL3vY_700bwp.webp");
+                return;
+            }
+
+            //base.Context.Guild.user
+
+            await Context.Channel.SendMessageAsync($"Creating {id} to {link}!");
+            await _linkShortenerService.ShortenUrl(id, _settings.AccessKey, link);
+            await Context.Channel.SendMessageAsync($"Create complete!");
         }
 
         // [Command("weather-legacy")]
@@ -68,66 +93,66 @@ namespace DiscordBot.Modules.CommandModules
         //     await Context.Channel.SendFileAsync(WeatherStream, $"{location}.png", temperature.ToString());
         // }
 
-    //     [Command("weather")]
-    //     public async Task GetWeatherAsync([Remainder] string location)
-    //     {
-    //         var weather = await _weatherService.GetWeatherAsync(location);
-    //         if (weather == null)
-    //         {
-    //             await ReplyAsync($"Für `{location}` wurde kein Wetter gefunden!");
-    //             return;
-    //         }
+        //     [Command("weather")]
+        //     public async Task GetWeatherAsync([Remainder] string location)
+        //     {
+        //         var weather = await _weatherService.GetWeatherAsync(location);
+        //         if (weather == null)
+        //         {
+        //             await ReplyAsync($"Für `{location}` wurde kein Wetter gefunden!");
+        //             return;
+        //         }
 
-    //         var eb = new EmbedBuilder()
-    //             .WithThumbnailUrl(weather.ThumbnailUrl)
-    //             .WithTitle(weather.Main)
-    //             .WithDescription(weather.Description)
-    //             .WithColor(GetEmbedColor((float)weather.TempAsDouble))
-    //             .AddField("Temparatur", weather.Temparature, true)
-    //             .AddField("RealFeel", weather.RealFeelTemp, true)
-    //             .AddField("\u200b", "\u200b", false)
-    //             .AddField("Luftfeuchtigkeit", weather.Humidity, true)
-    //             .AddField("Luftdruck", weather.Pressure, true)
-    //             ;
+        //         var eb = new EmbedBuilder()
+        //             .WithThumbnailUrl(weather.ThumbnailUrl)
+        //             .WithTitle(weather.Main)
+        //             .WithDescription(weather.Description)
+        //             .WithColor(GetEmbedColor((float)weather.TempAsDouble))
+        //             .AddField("Temparatur", weather.Temparature, true)
+        //             .AddField("RealFeel", weather.RealFeelTemp, true)
+        //             .AddField("\u200b", "\u200b", false)
+        //             .AddField("Luftfeuchtigkeit", weather.Humidity, true)
+        //             .AddField("Luftdruck", weather.Pressure, true)
+        //             ;
 
-    //         await ReplyAsync(embed: eb.Build());
-    //     }
+        //         await ReplyAsync(embed: eb.Build());
+        //     }
 
-    //     private Color GetEmbedColor(float temp)
-    //     {
-    //         float minTemp = -5;
-    //         float maxTemp = 40;
-    //         float span = maxTemp - minTemp;
+        //     private Color GetEmbedColor(float temp)
+        //     {
+        //         float minTemp = -5;
+        //         float maxTemp = 40;
+        //         float span = maxTemp - minTemp;
 
-    //         temp = (float)Limit(minTemp, temp, maxTemp);
-    //         float partOfColor = (float)Math.Round((temp - minTemp) / span, digits: 3);
-    //         var startColor = (r: 52, g: 152, b: 219);
-    //         var endColor = (r: 231, g: 76, b: 60);
+        //         temp = (float)Limit(minTemp, temp, maxTemp);
+        //         float partOfColor = (float)Math.Round((temp - minTemp) / span, digits: 3);
+        //         var startColor = (r: 52, g: 152, b: 219);
+        //         var endColor = (r: 231, g: 76, b: 60);
 
-    //         float red = CalcColorPart(startColor.r, endColor.r, partOfColor);
-    //         float green = CalcColorPart(startColor.g, endColor.g, partOfColor);
-    //         float blue = CalcColorPart(startColor.b, endColor.b, partOfColor);
-            
-    //         return new Color((int)Limit(0, Math.Round(red), 255), (int)Limit(0, Math.Round(green), 255), (int)Limit(0, Math.Round(blue), 255));
-    //     }
+        //         float red = CalcColorPart(startColor.r, endColor.r, partOfColor);
+        //         float green = CalcColorPart(startColor.g, endColor.g, partOfColor);
+        //         float blue = CalcColorPart(startColor.b, endColor.b, partOfColor);
 
-    //     private double Limit(double min, double given, double max)
-    //     {
-    //         return Math.Max(min, Math.Min(given, max));
-    //     }
+        //         return new Color((int)Limit(0, Math.Round(red), 255), (int)Limit(0, Math.Round(green), 255), (int)Limit(0, Math.Round(blue), 255));
+        //     }
 
-    //     private float CalcColorPart(float value1, float value2, float partOfColor)
-    //     {
-    //         float span = Math.Abs(value2 - value1);
-    //         float partOfSpan = partOfColor * span;
-    //         if (value2 > value1)
-    //         {
-    //             return value1 + partOfSpan;
-    //         }
-    //         else
-    //         {
-    //             return value1 - partOfSpan;
-    //         }
-    //     }
+        //     private double Limit(double min, double given, double max)
+        //     {
+        //         return Math.Max(min, Math.Min(given, max));
+        //     }
+
+        //     private float CalcColorPart(float value1, float value2, float partOfColor)
+        //     {
+        //         float span = Math.Abs(value2 - value1);
+        //         float partOfSpan = partOfColor * span;
+        //         if (value2 > value1)
+        //         {
+        //             return value1 + partOfSpan;
+        //         }
+        //         else
+        //         {
+        //             return value1 - partOfSpan;
+        //         }
+        //     }
     }
 }
